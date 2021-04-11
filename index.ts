2772eb987fb151d95ee45c487433b0eb39058b65
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-11 12:10:54 -0400

    feat: implement attributes flattening logic

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request } from "express";
 import multer from "multer";
 import { join } from "path";
+import { FlattenAttributesService } from "./src/Service/FlattenAttributesService";
 
 const app = express();
 
@@ -15,6 +16,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+enum TYPE {
+  PRIMITIVE = "Primitive",
+  OBJECT = "Object",
+  ARRAY = "Array",
+}
+
+const flattenAttributes: string[] = [];
+
 app.post("/json-upload", upload.single("file"), (req, res) => {
   const { file } = req;
 
@@ -22,28 +31,66 @@ app.post("/json-upload", upload.single("file"), (req, res) => {
 
   const inputJSON = require(filePath);
 
-  const keys = Object.keys(inputJSON);
+  Object.keys(inputJSON).forEach((key) => {
+    const attributesArray: string[] = [key];
 
-  const flattenAttributes: string[] = [];
-
-  keys.forEach((key) => {
     const value = inputJSON[key];
 
-    let attrString = key;
+    const valueType = detectValueType(value);
 
-    if (isArray(value)) {
-      console.log(`${key} --> Array`);
-    } else if (isObject(value)) {
-      console.log(`${key} --> Object`);
-
-      attrString = attrString + "." + completeAttrString(value, attrString);
+    if (valueType === TYPE.PRIMITIVE) {
+      flattenAttributes.push(attributesArray[0]);
     }
 
-    flattenAttributes.push(attrString);
+    if (valueType === TYPE.ARRAY) {
+      attributesArray.push("[Array]");
+      flattenAttributes.push(attributesArray.join("-"));
+    }
+
+    if (valueType === TYPE.OBJECT) {
+      handleObject(value, attributesArray);
+    }
   });
 
   return res.status(200).json(flattenAttributes);
 });
+
+function handleObject(obj: any, attributesArray: string[]) {
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+
+    const valueType = detectValueType(value);
+
+    if (valueType === TYPE.PRIMITIVE) {
+      attributesArray.push(key);
+      flattenAttributes.push(attributesArray.join("."));
+      attributesArray.pop();
+    }
+
+    if (valueType === TYPE.ARRAY) {
+      attributesArray.push(key + "[Array]");
+      flattenAttributes.push(attributesArray.join("."));
+      attributesArray.pop();
+    }
+
+    if (valueType === TYPE.OBJECT) {
+      attributesArray.push(key);
+      handleObject(value, attributesArray);
+      attributesArray.pop();
+    }
+  });
+
+  attributesArray.pop();
+}
+
+function detectValueType(value: any): string {
+  if (Array.isArray(value)) {
+    return TYPE.ARRAY;
+  } else if (value && typeof value === "object") {
+    return TYPE.OBJECT;
+  }
+  return TYPE.PRIMITIVE;
+}
 
 function isArray(attr: any): boolean {
   return Array.isArray(attr);
@@ -53,27 +100,35 @@ function isObject(attr: any): boolean {
   return typeof attr === "object";
 }
 
-function completeAttrString(obj: any, attrString: string): string {
+function completeAttrString(obj: any, attrString2 = "") {
+  let attrString = attrString2;
+
   const keys = Object.keys(obj);
 
-  if (keys) {
-    keys.forEach((key) => {
-      const value = obj[key];
+  keys.forEach((key, index) => {
+    const value = obj[key];
 
-      if (isArray(value)) {
-        attrString = attrString + "." + key;
-        return attrString;
-      } else if (isObject(value)) {
-        attrString = attrString + "." + completeAttrString(value, attrString);
-        return attrString;
-      }
+    if (isArray(value)) {
+      !attrString
+        ? flattenAttributes.push(`${key}[Array]`)
+        : flattenAttributes.push(attrString + `.${key}[Array]`);
+    } else if (value && isObject(value)) {
+      !attrString ? (attrString = attrString + `${key}`) : (attrString = attrString + `.${key}`);
+      completeAttrString(value, attrString);
+    } else {
+      !attrString
+        ? flattenAttributes.push(`${key}`)
+        : flattenAttributes.push(attrString + `.${key}`);
+    }
 
-      attrString = attrString + "." + key;
-      return attrString;
-    });
-  }
+    if (index === keys.length - 1) {
+      attrString = "";
+    }
+  });
 
-  return "";
+  attrString = "";
+
+  return flattenAttributes;
 }
 
 const port = 3000;
---
+++ src/Service/FlattenAttributesService.ts
@@ -0,0 +1,7 @@
+class FlattenAttributesService {
+  execute(objectInput: any) {
+    console.log("worked");
+  }
+}
+
+export { FlattenAttributesService };
